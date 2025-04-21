cd47b3cef16dc7e860d5585578d9255c488034df
umedzhan <karayev.umedjon@gmail.com>
2025-04-21 22:09:35 +0500

    update my-fun-facts

--- src/components/my-fun-facts.jsx
+++ src/components/my-fun-facts.jsx
@@ -10,15 +10,15 @@ export const MyFunFacts = () => {
       </h1>
       <div className="flex flex-col justify-between gap-[156px]">
         <div className="flex flex-wrap gap-3">
-          <BlockFacts Text="I like winter more than summer" />
-          <BlockFacts Text="I do'nt like to read books" />
-          <BlockFacts Text="I like to play video games" />
-          <BlockFacts Text="I like to play basketball" />
-          <BlockFacts Text="I like to play football" />
-          <BlockFacts Text="I like to play volleyball" />
-          <BlockFacts Text="I like to play basketball" />
-          <BlockFacts Text="I like to play football" />
-          <BlockFacts Text="I like to play volleyball" />
+          <BlockFacts Text="I like autumn more than other seasons" />
+          <BlockFacts Text="I don't enjoy reading literary books" />
+          <BlockFacts Text="I like learning new algorithms" />
+          <BlockFacts Text="I enjoy writing code" />
+          <BlockFacts Text="I'm interested in swimming, but I don't know how to swim yet" />
+          <BlockFacts Text="I don't like playing football" />
+          <BlockFacts Text="I don't like playing volleyball" />
+          <BlockFacts Text="I only enjoy reading books related to my field" />
+          <BlockFacts Text="I prefer spending time learning tech stuff over playing sports" />
         </div>
       </div>
     </div>
